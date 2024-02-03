01a892229e86e3f047bebd7b410a505b6e3112b8
sam-stewart-0311 <149884032+sam-stewart-0311@users.noreply.github.com.>
2024-02-03 15:13:19 +0000

    Add fucntion to play a round of the game

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const computerChoice = getComputerChoice();
+const playersChoice = prompt('Rock, Paper or Scissors?')
+let score = 0;
+
 function getComputerChoice() {
   let randomNumber = Math.random();
   
@@ -13,5 +17,49 @@ function getComputerChoice() {
   
 }
 
-const computerChoice = getComputerChoice();
-console.log(computerChoice);
+function playRound(playerChoice, computerChoice) {
+  let player = playerChoice.toLowerCase();
+  let computer = computerChoice.toLowerCase();
+  let result = '';
+  
+
+  if (player === 'rock') {
+    if (computer === 'rock') {
+      result = "It's a tie"
+    } else if (computer === 'paper') {
+      result = "You Lose"
+    } else if (computer === 'scissors') {
+      result = "You Win"
+    }
+  } else if (player === 'paper') {
+    if (computer === 'rock') {
+      result = "You Win"
+    } else if (computer === 'paper') {
+      result = "It's a Tie"
+    } else if (computer === 'scissors') {
+      result = "You Lose"
+    }
+  } else if (player === 'scissors') {
+    if (computer === 'rock') {
+      result = "You Lose"
+    } else if (computer === 'paper') {
+      result = "You Win"
+    } else if (computer === 'scissors') {
+      result = "It's a Tie"
+    } 
+  } else {
+    return 'You need to choose a valid option'
+  }
+  
+  player = player.charAt(0).toUpperCase() + player.slice(1);
+  computer = computer.charAt(0).toUpperCase() + computer.slice(1);
+
+  let message = `You chose ${player}, Computer chose ${computer}, ${result}`;
+  return message;
+}
+
+console.log(playRound(playersChoice, computerChoice));
+
+
+
+
