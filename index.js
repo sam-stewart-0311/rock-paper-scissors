b5b7387e17cb94c1293b885d8717ab450525fbe5
sam-stewart-0311 <149884032+sam-stewart-0311@users.noreply.github.com.>
2024-02-03 14:39:00 +0000

    Add function to get Computers choice

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+function getComputerChoice() {
+  let randomNumber = Math.random();
+  
+  if (randomNumber > 0 && randomNumber < 1/3) {
+    return 'Rock';
+  } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+    return 'Paper';
+  } else if (randomNumber >= 2/3 && randomNumber <= 1) {
+    return 'Scissors'
+  } else {
+    throw Error("No Computer Choice");
+  }
+  
+}
+
+const computerChoice = getComputerChoice();
+console.log(computerChoice);
