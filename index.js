73e39adec4f99621e8fcf704a87869964d6b466b
sam-stewart-0311 <149884032+sam-stewart-0311@users.noreply.github.com.>
2024-02-23 15:42:34 +0000

    Added function that plays a round of the game

--- index.js
+++ index.js
@@ -1,15 +1,65 @@
+const playerChoice = '';
+const computerChoice = getComputerChoice();
+
 function getComputerChoice() {
   let randomNumber = Math.random();
   let result = '';
 
   if (randomNumber < 1/3) {
-    result = 'Rock';
+    result = 'rock';
   } else if (randomNumber >= 1/3 && randomNumber < 2/3) {
-    result = 'Paper';
+    result = 'paper';
   } else if (randomNumber >= 2/3 && randomNumber < 1) {
-    result = 'Scissors';
+    result = 'scissors';
   }
   return result;
 };
 
-console.log(getComputerChoice());
+function playRound(playerChoice, computerChoice) {
+  playerChoice = prompt();
+  computerChoice = getComputerChoice();
+
+  let playerChoiceLowerCase = playerChoice.toLowerCase();
+  let result = '';
+  
+  if (playerChoiceLowerCase === 'rock') {
+
+    if (computerChoice === 'rock') {
+      result = "It's a Tie";
+    } else if (computerChoice === 'paper') {
+      result = "You Lose"
+    } else if (computerChoice === 'scissors') {
+      result = "You Win"
+    }
+
+  } else if (playerChoiceLowerCase === 'paper') {
+
+    if (computerChoice === 'rock') {
+      result = "You Win";
+    } else if (computerChoice === 'paper') {
+      result = "It's a Tie"
+    } else if (computerChoice === 'scissors') {
+      result = "You Lose"
+    }
+
+  } else if (playerChoiceLowerCase === 'scissors') {
+
+    if (computerChoice === 'rock') {
+      result = "You Lose";
+    } else if (computerChoice === 'paper') {
+      result = "You Win"
+    } else if (computerChoice === 'scissors') {
+      result = "It's a Tie"
+    }
+
+  } else {
+    return 'You need to choose Rock, Paper or Scissors'
+  }
+
+  let message = `You chose ${playerChoice}, the Computer chose ${computerChoice}, ${result}!`;
+
+  return message;
+};
+
+console.log(playRound(playerChoice, computerChoice));
+
